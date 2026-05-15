6c6d8e3cda19e62805889170a31c3cfe76b7e46a
Anderson Gutemberg <anderson_gutemberg@hotmail.com>
2026-05-15 13:45:30 -0300

    Criando o formulário da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,38 @@ export function App() {
   return (
     <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
 
       <Container>
-        <Menu/>
+        <Menu />
       </Container>
 
       <Container>
-        <CountDown  />
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>Task:</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+          
+        </form>
       </Container>
     </>
   );
