c0472968f31ee36e8400d614a186570deca22484
Anderson Gutemberg <anderson_gutemberg@hotmail.com>
2026-05-28 08:27:26 -0300

    Capturando o evento de clique em link com preventDefault

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -12,15 +12,28 @@ export function Menu() {
   ) {
     event.preventDefault();
 
-    console.log('Clique', Date.now());
-
     setTheme(prevTheme => {
       const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return newTheme;
     });
-
-     //document.documentElement.setAttribute('data-theme', theme);
   }
+
+  /*  useEffect(() => {
+    console.log('', Date.now());
+  });
+
+   useEffect(() => {
+    console.log('', Date.now());
+  }, []);
+ */
+  useEffect(() => {
+    console.log('theme mudou', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('Olha, este componente será atualizado');
+    };
+  }, [theme]);
 
   return (
     <nav className={styles.menu}>
