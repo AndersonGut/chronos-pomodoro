0a467f8e3819b712a4becc0552b77dd51d687728
Anderson Gutemberg <anderson_gutemberg@hotmail.com>
2026-06-02 15:02:28 -0300

    Estado global da aplicação com useState

--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,8 +1,8 @@
 import type { TaskModel } from './TaskModel';
 
 export type TaskStateModel = {
-  tasks: TaskModel[];
-  secondsRemaining: number;
+  tasks: TaskModel[]; 
+  secondsRemaining: number; 
   formattedSecondsRemaining: string;
   activeTask: TaskModel | null;
   currentCycle: number;
