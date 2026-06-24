cabb4bca36abf8324ee3c91bf28653b3204b7ebf
Anderson Gutemberg <anderson_gutemberg@hotmail.com>
2026-06-24 16:09:09 -0300

    Organizando o contexto em arquivos separados

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Home } from './pages/Home';
-import { TaskContextProvider } from './contexts/TaskContext';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 export function App() {
   return (
-  <TaskContextProvider>
-    <Home />
-  </TaskContextProvider>
-);
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
