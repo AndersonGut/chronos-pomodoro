492977ff46e3d8edca6f418575cfd524018de2d8
Anderson Gutemberg <anderson_gutemberg@hotmail.com>
2026-08-16 15:27:32 -0300

    Entendendo o mínimo necessário sobre Web Workers

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { taskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../tips';
+
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
@@ -40,6 +41,19 @@ export function MainForm() {
     };
 
     dispatch({ type: taskActionTypes.START_TASK, payload: newTask });
+
+    const worker = new Worker(
+      new URL('../../worKers/timeWorker.js', import.meta.url),
+    );
+
+    worker.postMessage('FAVOR'); // Sim, posso fazer um favor
+    worker.postMessage('FALA_OI'); // OK: OI!
+    worker.postMessage('BLABLA'); // Não entendi!
+    worker.postMessage('FECHAR'); // Tá bom, vou fechar
+
+    worker.onmessage = function (event) {
+      console.log('PRINCIPAL recebeu:', event.data);
+    }
   }
 
   function handleInterruptTask() {
